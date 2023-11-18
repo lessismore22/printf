f34e4b57b045002b5a044a4deae0e9bcf00b2cfd
Mattymatics <iyogunmathias@gmail.com>
2023-11-17 20:20:44 -0800

    print function update

--- _printf.c
+++ _printf.c
@@ -28,7 +28,7 @@ int _printf(const char *format, ...)
 Here:
 	while (format[i] != '\0')
 	{
-		j = 4;
+		j = 12;
 		while (j >= 0)
 		{
 			if (st[j].id[0] == format[i] && st[j].id[1] == format[i + 1])
